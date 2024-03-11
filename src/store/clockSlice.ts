f5c2011a4a693591e93081eae8f285332c24ff21
bora001 <bora.vancouver@gmail.com>
2024-03-11 21:20:21 +0900

    rtk 정리

--- src/store/clockSlice.ts
+++ src/store/clockSlice.ts
@@ -1,17 +1,13 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { RootState } from './store';
 
-// Define a type for the slice state
 interface ClockSlice {
- value: number;
  hour: number;
  minute: number;
  second: number;
 }
 
-// Define the initial state using that type
 const initialState: ClockSlice = {
- value: 0,
  hour: new Date().getHours(),
  minute: new Date().getMinutes(),
  second: new Date().getSeconds(),
@@ -21,9 +17,6 @@ export const clockSlice = createSlice({
  name: 'clock',
  initialState,
  reducers: {
-  increment: (state) => {
-   state.value += 1;
-  },
   setHour: (state, action: PayloadAction<number>) => {
    state.hour = action.payload;
   },
@@ -36,9 +29,8 @@ export const clockSlice = createSlice({
  },
 });
 
-export const { increment, setHour, setMinutes, setSeconds } = clockSlice.actions;
+export const { setHour, setMinutes, setSeconds } = clockSlice.actions;
 
-// Other code such as selectors can use the imported `RootState` type
 export const clockValue = (state: RootState) => state.clock;
 
 export default clockSlice.reducer;
