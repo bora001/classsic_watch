f30097c06fc3b82ce32ed4e6bc747f7c2d75f99b
bora001 <bora.vancouver@gmail.com>
2024-03-11 21:53:52 +0900

    시침 강조

--- src/common/TimeBar.tsx
+++ src/common/TimeBar.tsx
@@ -15,15 +15,16 @@ const TimeBarComponent = styled.span<TimeBarComponentProps>`
   left: calc(50% - 1.5px);
   position: absolute;
   width: 3px;
+  width: ${({ color }) => `${color ? 4 : 1.5}px`};
   top: ${({ height }) => `${150 - height}px`};
   height: ${({ height }) => `${height}px`};
-  background: #000;
+  background: ${({ color }) => `${color ?? '#000'}`};
   content: '';
  }
  transform: ${({ $degree }) => $degree};
 `;
-const TimeBar = ({ degree, height }: { degree: string; height: number }) => {
- return <TimeBarComponent height={height} $degree={degree} />;
+const TimeBar = ({ degree, height, color }: { degree: string; height: number; color?: string }) => {
+ return <TimeBarComponent height={height} $degree={degree} color={color} />;
 };
 
 export default TimeBar;
--- src/component/ClockHand/HourHand.tsx
+++ src/component/ClockHand/HourHand.tsx
@@ -5,7 +5,7 @@ const HourHand = () => {
  const hour = useAppSelector((state) => state.clock.hour);
  return (
   <div>
-   <TimeBar height={85} degree={`rotate(${(360 / 12) * hour}deg)`} />
+   <TimeBar height={80} degree={`rotate(${(360 / 12) * hour}deg)`} color="red" />
   </div>
  );
 };
